10557fd718cbe09d633f6b23332209ceb9529085
Paweł Drapiewski <drapek39@gmail.com>
2018-03-10 22:23:00 +0100

    done river1 exercise

--- lab_03/lab_03_river1.m
+++ lab_03/lab_03_river1.m
@@ -9,43 +9,48 @@ close all;
 plot_bound_x_low = 0;
 plot_bound_x_high = 5;
 
-% to s¹ wektory normalne prostych okreœlaj¹cych rzêkê
+% Define the normal vectors of the river bounds (r1 and r1)
 A = [-2 1; -2 1];
 b = [-1 -3]';
+
+% define the cities
+a_point = [1.5, 4.5];
+b_point = [3.5, 0.5];
+
+% find the f line (perpendicular to the river) 
+f_normal_vec = [A(1, 2), -1 * A(1, 1)];
+
+% find the points for the bridge, where the cost will be the smaller
+% we should minimize the total length of the route. We know that building
+% the bridges is very expesive, so the bridge should be perpendicular to
+% river what provides the f line. Because of that fact we can ommit the
+% bridge length in minimalizing. So overall we need find optiab b value.
+cvx_begin
+    variable f_b(1)
+    variable p1(2)
+    variable p2(2)
+    minimize norm(a_point' - p1) + norm(b_point' - p2);
+        subject to
+            % find points where straight f is crossing with r1 (upper river bound) and r2 (bottom river bound)
+            p1 == find_intersection([A(1, 1) A(1, 2) b(1)], [f_normal_vec(1), f_normal_vec(2), f_b(1)])' % crossing of the r1 and f function
+            p2 == find_intersection([A(2, 1) A(2, 2) b(2)], [f_normal_vec(1), f_normal_vec(2), f_b(1)])' % crossing of the r2 and f function
+cvx_end
+
+
+% find cooridnates for the plotting the river
 x_low = plot_bound_x_low; x_high = plot_bound_x_high;
 y_low = (b - A(:,1)*x_low)./A(:,2);
 y_high = (b - A(:,1)*x_high)./A(:,2);
 matrix_A_size = size(A, 1);
 X = [x_low; x_high]*ones(1, matrix_A_size);
 Y = [y_low' ; y_high'];
-
-% define the cities
-a_point = [1.5, 4.5];
-b_point = [3.5, 0.5];
-
-
-
-% find the f line (perpendicular to the river) 
-% using this equation 
-f_normal_vec = [A(1, 2), -1 * A(1, 1)];
-f_b_val = [6]';
-
-% find the minimal cost
-cvx_begin
-    variable b_f
-    % find points where straight f is crossing with r1 (upper river bound) and r2 (bottom river bound)
-    p1 = find_intersection([A(1, 1) A(1, 2) b(1)], [f_normal_vec(1), f_normal_vec(2), f_b_val(1)]); % crossing of the r1 and f function
-    p2 = find_intersection([A(2, 1) A(2, 2) b(2)], [f_normal_vec(1), f_normal_vec(2), f_b_val(1)]); % crossing of the r2 and f function
-cvx_end
-
-% find cooridnates for the plotting the f_line
+% find cooridnates for the plotting the f line (red dashed)
 x_low_f = plot_bound_x_low; x_high_f = plot_bound_x_high;
-y_low_f = (f_b_val - f_normal_vec(:,1)*x_low_f)./f_normal_vec(:,2);
-y_high_f = (f_b_val - f_normal_vec(:,1)*x_high_f)./f_normal_vec(:,2);
+y_low_f = (f_b - f_normal_vec(:,1)*x_low_f)./f_normal_vec(:,2);
+y_high_f = (f_b - f_normal_vec(:,1)*x_high_f)./f_normal_vec(:,2);
 matrix_A_size = size(f_normal_vec, 1);
 f_X = [x_low_f; x_high_f]*ones(1, matrix_A_size);
 f_Y = [y_low_f' ; y_high_f'];
-
 
 % draw the plot
 hold on;
@@ -69,6 +74,5 @@ hold off;
 function result = find_intersection(line_1, line_2)
     y = (line_2(3) - line_2(1) * line_1(3) / line_1(1)) / (line_2(2) - line_2(1) * line_1(2) / line_1(1));
     x = line_1(3) / line_1(1) - line_1(2) / line_1(1) * y;
-    
     result = [x, y];
 end
