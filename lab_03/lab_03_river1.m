4d3c1b393de731c9375ecd6bba7ce91a4fd9c14e
Paweł Drapiewski <drapek39@gmail.com>
2018-03-12 20:23:49 +0100

    Almost done excercises for 3rd laboratory

--- lab_03/lab_03_river1.m
+++ lab_03/lab_03_river1.m
@@ -1,30 +1,30 @@
-%% Searching for optimal bridge place - RIVER_1
+%% Lab03 - River 1, Pawel Drapiewski, 11.03.2018 r.
+
+%% Searching for place to build bridge - RIVER 1
 % In this problem we will be searching for the most optimal place for the
 % bridge between city A and city B.
-% Author: Pawe³ Drapiewski
-
 clear all;
 close all;
 
+%% Define initial variables
 plot_bound_x_low = 0;
 plot_bound_x_high = 5;
-
 % Define the normal vectors of the river bounds (r1 and r1)
 A = [-2 1; -2 1];
 b = [-1 -3]';
-
 % define the cities
 a_point = [1.5, 4.5];
 b_point = [3.5, 0.5];
-
 % find the f line (perpendicular to the river) 
 f_normal_vec = [A(1, 2), -1 * A(1, 1)];
 
-% find the points for the bridge, where the cost will be the smaller
-% we should minimize the total length of the route. We know that building
-% the bridges is very expesive, so the bridge should be perpendicular to
-% river what provides the f line. Because of that fact we can ommit the
-% bridge length in minimalizing. So overall we need find optiab b value.
+%% Find the points where the bridge should be builded.
+% Find the points for the bridge, where the cost will be the smallest.
+% To gain it, we should minimize the total length of the route. And we know
+% that building the  bridge is very  expesive, so the best scenarios is
+% when bridge is perpendicular to river (f line). Because of that fact that
+% bridge lenght is constans we can ommit the bridge length in minimalizing.
+% So overall we need to find only optimal b value of the f line.
 cvx_begin
     variable f_b(1)
     variable p1(2)
@@ -36,8 +36,14 @@ cvx_begin
             p2 == find_intersection([A(2, 1) A(2, 2) b(2)], [f_normal_vec(1), f_normal_vec(2), f_b(1)])' % crossing of the r2 and f function
 cvx_end
 
+disp('Found route has the length: ')
+disp(norm(a_point' - p1) + norm(b_point' - p1) + norm(p2 - p1))
+disp('Bridge points are: ');
+disp(p1);
+disp(p2);
 
-% find cooridnates for the plotting the river
+%% Prepare data to be visulased on the plot
+% find river coordinates, so it could be drawn on the plot
 x_low = plot_bound_x_low; x_high = plot_bound_x_high;
 y_low = (b - A(:,1)*x_low)./A(:,2);
 y_high = (b - A(:,1)*x_high)./A(:,2);
@@ -45,6 +51,7 @@ matrix_A_size = size(A, 1);
 X = [x_low; x_high]*ones(1, matrix_A_size);
 Y = [y_low' ; y_high'];
 % find cooridnates for the plotting the f line (red dashed)
+% find f (red dashed line) coordinates, so it could be drawn on the plot
 x_low_f = plot_bound_x_low; x_high_f = plot_bound_x_high;
 y_low_f = (f_b - f_normal_vec(:,1)*x_low_f)./f_normal_vec(:,2);
 y_high_f = (f_b - f_normal_vec(:,1)*x_high_f)./f_normal_vec(:,2);
@@ -70,9 +77,37 @@ plot([b_point(1), p2(1)], [b_point(2), p2(2)], 'black')
 plot([x_low_f(1), x_high_f(1)], [y_low_f(1), y_high_f(1)], 'red--')
 hold off;
 
+%% Solve the problem using fmincon function
+d = norm(p2 - p1);
+fmin_A = [ A(1, 1) A(1, 2) 0 0;
+           0 0 A(1, 1) A(1, 2);
+           A(2, 1) A(2, 2) 0 0;
+           0 0 A(2, 1) A(2, 2)];
+b = [-1; -3; d; d];
+f_descriptor = @(x) double(min_route(x));
+x0 = [0, 0, 0 ,0];
+X = fmincon(f_descriptor, x0, [], [], fmin_A, b);
+
+disp("Solution using fmincon: ");
+disp("p1: ");
+disp(X(1:2));
+disp("p2: ");
+disp(X(3:4));
+
+% As we can see, this result is only aproximation of the proper value and 
+% the cvx was better in that case.
+
 % function to find the intersection point of two lines
 function result = find_intersection(line_1, line_2)
     y = (line_2(3) - line_2(1) * line_1(3) / line_1(1)) / (line_2(2) - line_2(1) * line_1(2) / line_1(1));
     x = line_1(3) / line_1(1) - line_1(2) / line_1(1) * y;
     result = [x, y];
+end
+
+% This is hardcoded problem of the upper example. This function exist for
+% fmincon calculation only.
+function res = min_route(x)
+    a_point = [1.5, 4.5];
+    b_point = [3.5, 0.5];
+    res = norm([a_point(1)-x(1), a_point(2)-x(2)]) + norm([b_point(1)-x(3) b_point(2)-x(4)]) + norm([x(1)-x(3), x(2)-x(4)]);
 end
--- lab_03/lab_03_river2.m
+++ lab_03/lab_03_river2.m
@@ -1,4 +1,6 @@
-%% Searching for optimal bridge place - RIVER_2
+%% Lab03 - River 2, Pawel Drapiewski, 11.03.2018 r.
+
+%% Searching for optimal bridge place - RIVER 2
 % In this problem we will be searching for the most optimal place for the
 % bridge between city A and city B. And the river is desricbed by circle
 % Author: Pawe³ Drapiewski
@@ -9,7 +11,7 @@ close all;
 plot_bound_x_low = 0;
 plot_bound_x_high = 5;
 
-% Define the paramters for the river
+%% Define initial variables
 O = [1, 5]; % the center of the river curve
 r1 = 2;
 r2 = 3;
@@ -18,12 +20,24 @@ r2 = 3;
 a_point = [2, 5];
 b_point = [3, 1];
 
-optimal_th = 0;
+% optimal_th = 0;
+% p1 = get_point_on_circle_based_on_angle(O, r1, optimal_th);
+% p2 = get_point_on_circle_based_on_angle(O, r2, optimal_th);
+%% CVX - find the optimal place for a bridge
+cvx_solver('sdpt3')
+cvx_begin
+    variable optimal_th
+    variable p1(2)
+    variable p2(2)
+    minimize norm(a_point' - p1) + norm(b_point' - p2);
+        subject to
+           p1 = get_point_on_circle_based_on_angle(O, r1, optimal_th);
+           p2 = get_point_on_circle_based_on_angle(O, r2, optimal_th);
+           optimal_th > 0;
+           optimal_th < 2 * pi;
+cvx_end
 
-p1 = get_circle_value_pair(O, r1, optimal_th);
-p2 = get_circle_value_pair(O, r2, optimal_th);
-
-% find cooridnates for the plotting the river
+%% Prepare data to be visulased on the plot
 % Circle 1
 th = 0:pi/50:2*pi;
 x_circle1 = r1 * cos(th) + O(1);
@@ -32,7 +46,6 @@ y_circle1 = r1 * sin(th) + O(2);
 th = 0:pi/50:2*pi;
 x_circle2 = r2 * cos(th) + O(1);
 y_circle2 = r2 * sin(th) + O(2);
-
 % find cooridnates for ploting the dashed line (f)
 m = (p2(2)-p2(2))/(p2(1)-p1(1));
 n = p1(2)- p1(1)*m;
@@ -61,8 +74,9 @@ plot(b_point(1), b_point(2) ,'ro');
 
 hold off;
 
-function result = get_circle_value_pair(O, r, th)
+function result = get_point_on_circle_based_on_angle(O, r, th)
     x = r * cos(th) + O(1);
     y = r * sin(th) + O(2);
     result = [x, y];
 end
+
