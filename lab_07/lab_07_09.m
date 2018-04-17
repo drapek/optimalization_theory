bbc37755a7cb3bf1cd92079c86704e501622b0d6
Paweł Drapiewski <drapek39@gmail.com>
2018-04-17 20:59:29 +0200

    Done exercise number 9

--- lab_07/lab_07_09.m
+++ lab_07/lab_07_09.m
@@ -2,3 +2,22 @@
 %% 
 % 9th excercise from Optimalization_problems.pdf
 
+% The biggest possible rectangle drawn into circle of radius 4
+% x, y - sides of the rectangle
+
+% Solution
+clear, close all;
+
+cvx_begin 
+variables v 
+maximize 256 * v - 16 * v * v
+subject to
+    - v <= 0
+cvx_end
+
+u = 16 - v;
+x = sqrt(u);
+y = sqrt(v);
+
+fprintf('x=%2.4f, y=%2.4f \n', x, y)
+fprintf('Rectnalge area: %2.4f \n', 2 * x + 2 * y)
