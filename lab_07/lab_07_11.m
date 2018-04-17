6edb222e78cab7909ad150a18be75d57acd8e2ab
Paweł Drapiewski <drapek39@gmail.com>
2018-04-17 22:13:03 +0200

    Done exercise 11 (without gp)

--- lab_07/lab_07_11.m
+++ lab_07/lab_07_11.m
@@ -1,3 +1,17 @@
 %% Lab07 - optimalization exercises, Pawel Drapiewski 17.04.2018 r.
 %% 
 % 11th excercise from Optimalization_problems.pdf
+
+% River - the fastest way to reach point on the opposite bank
+
+% Solution
+clear, close all;
+
+cvx_begin 
+variables x 
+minimize (norm([x 9]) / 6 - x/8)
+subject to
+    - x <= 0
+cvx_end
+
+fprintf('x=%2.4f \n', x)
