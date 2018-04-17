615af4f1377005d9cceec5a90899d7f81ad69d69
Paweł Drapiewski <drapek39@gmail.com>
2018-04-17 22:29:51 +0200

    Done exercise number 10

--- lab_07/lab_07_10.m
+++ lab_07/lab_07_10.m
@@ -1,3 +1,18 @@
 %% Lab07 - optimalization exercises, Pawel Drapiewski 17.04.2018 r.
 %% 
 % 10th excercise from Optimalization_problems.pdf
+
+% Solution
+clear, close all;
+
+cvx_solver sdpt3
+cvx_begin 
+variables y
+minimize y * y - 3 * y + 3
+subject to
+    - y <= 0
+cvx_end
+
+x_p2 = y - 1;
+x = sqrt(x_p2);
+fprintf('x=%2.4f \n', x)
